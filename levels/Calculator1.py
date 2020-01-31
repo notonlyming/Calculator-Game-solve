7122a52009a2c9cc56a6ef73e25898e3d200ab53
notonlyming <42286434+notonlyming@users.noreply.github.com>
2020-01-31 11:02:26 +0800

    更新到108关

--- levels/Calculator1.py
+++ levels/Calculator1.py
@@ -405,7 +405,471 @@
         "achieve":"126",
         "start":"111",
         "max_step":"6",
-        "buttons":"*-3 -9 +/- <<",
+        "buttons":"*3 -9 +/- <<",
+        "protal":"n"
+    },
+    {
+        "name":"Level 51",
+        "achieve":"3",
+        "start":"34",
+        "max_step":"5",
+        "buttons":"-5 +8 /7 +/-",
+        "protal":"n"
+    },
+    {
+        "name":"Level 52",
+        "achieve":"4",
+        "start":"25",
+        "max_step":"5",
+        "buttons":"-4 *-4 /3 /8 +/-",
+        "protal":"n"
+    },
+    {
+        "name":"Level 53",
+        "achieve":"21",
+        "start":"12",
+        "max_step":"1",
+        "buttons":"reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 54",
+        "achieve":"51",
+        "start":"0",
+        "max_step":"3",
+        "buttons":"+6 +9 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 55",
+        "achieve":"101",
+        "start":"100",
+        "max_step":"3",
+        "buttons":"1 +9 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 56",
+        "achieve":"100",
+        "start":"1101",
+        "max_step":"4",
+        "buttons":"-1 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 57",
+        "achieve":"58",
+        "start":"0",
+        "max_step":"4",
+        "buttons":"+4 *4 -3 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 58",
+        "achieve":"4",
+        "start":"6",
+        "max_step":"3",
+        "buttons":"1 /4 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 59",
+        "achieve":"21",
+        "start":"15",
+        "max_step":"3",
+        "buttons":"+9 *5 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 60",
+        "achieve":"13",
+        "start":"100",
+        "max_step":"5",
+        "buttons":"/2 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 61",
+        "achieve":"11011",
+        "start":"10",
+        "max_step":"4",
+        "buttons":"1 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 62",
+        "achieve":"102",
+        "start":"0",
+        "max_step":"4",
+        "buttons":"10 *4 +5 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 63",
+        "achieve":"7",
+        "start":"0",
+        "max_step":"4",
+        "buttons":"2 +1 /3 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 64",
+        "achieve":"4",
+        "start":"0",
+        "max_step":"4",
+        "buttons":"5 *4 *2 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 65",
+        "achieve":"212",
+        "start":"121",
+        "max_step":"3",
+        "buttons":"2 -1 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 66",
+        "achieve":"9",
+        "start":"8",
+        "max_step":"5",
+        "buttons":"*3 1 /5 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 67",
+        "achieve":"13",
+        "start":"0",
+        "max_step":"5",
+        "buttons":"+7 +8 +9 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 68",
+        "achieve":"123",
+        "start":"0",
+        "max_step":"6",
+        "buttons":"+3 1 -2 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 69",
+        "achieve":"424",
+        "start":"0",
+        "max_step":"5",
+        "buttons":"6 +8 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 70",
+        "achieve":"81",
+        "start":"7",
+        "max_step":"5",
+        "buttons":"-9 *3 +4 +/- reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 71",
+        "achieve":"-43",
+        "start":"0",
+        "max_step":"5",
+        "buttons":"-5 +7 -9 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 72",
+        "achieve":"28",
+        "start":"0",
+        "max_step":"7",
+        "buttons":"+6 -3 << reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 73",
+        "achieve":"136",
+        "start":"0",
+        "max_step":"5",
+        "buttons":"1 +2 *3 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 74",
+        "achieve":"-1",
+        "start":"0",
+        "max_step":"4",
+        "buttons":"+5 +/- reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 75",
+        "achieve":"-25",
+        "start":"0",
+        "max_step":"5",
+        "buttons":"+4 *3 +/- reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 76",
+        "achieve":"-5",
+        "start":"0",
+        "max_step":"5",
+        "buttons":"+7 *3 +/- reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 77",
+        "achieve":"41",
+        "start":"88",
+        "max_step":"4",
+        "buttons":"/4 -4 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 78",
+        "achieve":"101",
+        "start":"100",
+        "max_step":"5",
+        "buttons":"0 *2 2=>10 0=>1 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 79",
+        "achieve":"424",
+        "start":"0",
+        "max_step":"7",
+        "buttons":"/2 5 5=>4 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 80",
+        "achieve":"100",
+        "start":"99",
+        "max_step":"5",
+        "buttons":"9 /9 1=>0 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 81",
+        "achieve":"30",
+        "start":"8",
+        "max_step":"5",
+        "buttons":"2 -4 2=>3 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 82",
+        "achieve":"222",
+        "start":"101",
+        "max_step":"5",
+        "buttons":"-1 0=>2 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 83",
+        "achieve":"500",
+        "start":"36",
+        "max_step":"5",
+        "buttons":"*4 /3 1=>5 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 84",
+        "achieve":"196",
+        "start":"0",
+        "max_step":"8",
+        "buttons":"1 +12 *13 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 85",
+        "achieve":"101",
+        "start":"50",
+        "max_step":"5",
+        "buttons":"1=>10 +50 5=>1 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 86",
+        "achieve":"2048",
+        "start":"1",
+        "max_step":"6",
+        "buttons":"2 *4 *10 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 87",
+        "achieve":"123",
+        "start":"12",
+        "max_step":"5",
+        "buttons":"12 +1 12=>2 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 88",
+        "achieve":"55",
+        "start":"86",
+        "max_step":"6",
+        "buttons":"+2 +14 0=>5 reverse",
+        "protal":"n"
+    },
+    {
+        "name":"Level 89",
+        "achieve":"3",
+        "start":"0",
+        "max_step":"4",
+        "buttons":"1 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 90",
+        "achieve":"4",
+        "start":"1231",
+        "max_step":"3",
+        "buttons":"3=>1 2=>3 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 91",
+        "achieve":"45",
+        "start":"0",
+        "max_step":"5",
+        "buttons":"*9 4 *3 3=>5 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 92",
+        "achieve":"28",
+        "start":"424",
+        "max_step":"5",
+        "buttons":"*4 4=>6 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 93",
+        "achieve":"8",
+        "start":"3",
+        "max_step":"4",
+        "buttons":"3 +33 3=>1 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 94",
+        "achieve":"44",
+        "start":"24",
+        "max_step":"4",
+        "buttons":"/2 4 1=>2 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 95",
+        "achieve":"143",
+        "start":"142",
+        "max_step":"4",
+        "buttons":"*9 +9 44=>43 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 96",
+        "achieve":"1",
+        "start":"24",
+        "max_step":"5",
+        "buttons":"/3 *4 5=>10 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 97",
+        "achieve":"100",
+        "start":"4",
+        "max_step":"5",
+        "buttons":"3 *3 +1 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 98",
+        "achieve":"8",
+        "start":"93",
+        "max_step":"5",
+        "buttons":"+4 *3 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 99",
+        "achieve":"16",
+        "start":"5",
+        "max_step":"5",
+        "buttons":"*5 /2 5=>2 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 100",
+        "achieve":"64",
+        "start":"128",
+        "max_step":"4",
+        "buttons":"*4 /4 5=>16 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 101",
+        "achieve":"121",
+        "start":"59",
+        "max_step":"6",
+        "buttons":"1 *5 15=>51 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 102",
+        "achieve":"5",
+        "start":"18",
+        "max_step":"6",
+        "buttons":"*2 /3 12=>21 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 103",
+        "achieve":"30",
+        "start":"9",
+        "max_step":"4",
+        "buttons":"-5 *-6 +/- sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 104",
+        "achieve":"-17",
+        "start":"105",
+        "max_step":"5",
+        "buttons":"-5 /5 *4 +/- sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 105",
+        "achieve":"11",
+        "start":"36",
+        "max_step":"6",
+        "buttons":"-6 /3 +/- sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 106",
+        "achieve":"64",
+        "start":"3",
+        "max_step":"5",
+        "buttons":"+3 x^3 0=>1 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 107",
+        "achieve":"11",
+        "start":"2",
+        "max_step":"5",
+        "buttons":"*2 10 10=>1 x^3 sum",
+        "protal":"n"
+    },
+    {
+        "name":"Level 108",
+        "achieve":"2311",
+        "start":"1123",
+        "max_step":"2",
+        "buttons":"shift<",
         "protal":"n"
     },
 ]
