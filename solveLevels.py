ef5c33a8aa5e423a8bc313e4c5605e923bcc91be
notonlyming <42286434+notonlyming@users.noreply.github.com>
2020-01-31 18:01:19 +0800

    添加小点点输出

--- solveLevels.py
+++ solveLevels.py
@@ -3,6 +3,23 @@
 
 import sys
 import os
+import time
+import threading
+
+class printDotThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.running = True
+
+    def run(self):
+        while self.running:
+            print('.', end='')
+            sys.stdout.flush()
+            time.sleep(1)
+    
+    def stopRun(self):
+        print("")
+        self.running = False
 
 if __name__ == '__main__':
     path = sys.argv[1]
@@ -18,14 +35,17 @@ if __name__ == '__main__':
     for level in levels:
         with open(outputFilePath, mode='a', encoding='utf-8') as f:
             f.write('### ' + level['name'] + '\n')
-        print(level["name"])
+        print(level["name"], end='\t')
         # 要构造的输入数据
         levelInfoInput = '{}\n{}\n{}\n{}\n{}\nn\n'.format(
             level['achieve'], level['start'], level["max_step"], level["buttons"], level["protal"])
         with open(inputFilePath, mode='w', encoding='utf-8') as f:
             f.write(levelInfoInput)
         
+        # 使用线程输出小点点
+        printThread = printDotThread()
+        printThread.start()
         # 调用一次程序解算
-        # print("cat {} | ./calculator_game --silent >> {}".format(inputFilePath, outputFilePath))
         os.system("cat {} | ./calculator_game --silent >> {}".format(inputFilePath, outputFilePath))
+        printThread.stopRun()
         
